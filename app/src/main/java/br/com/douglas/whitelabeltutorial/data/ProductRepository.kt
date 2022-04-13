35fa22d34d517faa0faa3e026970952e3d4cb95f
dougborbabass <douglas.borba@acad.pucrs.br>
2022-04-13 15:06:26 -0300

    adicionado modules para dataSource Firebase

--- app/src/main/java/br/com/douglas/whitelabeltutorial/data/ProductRepository.kt
+++ app/src/main/java/br/com/douglas/whitelabeltutorial/data/ProductRepository.kt
@@ -2,9 +2,11 @@ package br.com.douglas.whitelabeltutorial.data
 
 import android.net.Uri
 import br.com.douglas.whitelabeltutorial.domain.model.Product
+import javax.inject.Inject
 
 //O Repositorio depende de uma abstracao e nao de uma implementacao concreta
-class ProductRepository(private val dataSource: ProductDataSource) {
+class ProductRepository @Inject constructor(
+    private val dataSource: ProductDataSource) {
 
     //Para cada um dos casos de uso, devemos ter uma funcao no repositorio
     //O repositorio existe por conta dos casos de uso
